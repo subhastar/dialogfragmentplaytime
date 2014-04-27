c570544ee8e53fa12b814158d49ee4813d1cef28
Subha Gollakota <subha.gollakota@gmail.com>
2014-04-27 15:19:32 -0700

    first commit

---
+++ src/com/subha/modals/MainActivity.java
@@ -0,0 +1,49 @@
+package com.subha.modals;
+
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends FragmentActivity {
+	int mStackLevel = 0;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+	public void showDialog(View v) {
+	    mStackLevel++;
+
+	    // DialogFragment.show() will take care of adding the fragment
+	    // in a transaction.  We also want to remove any currently showing
+	    // dialog, so make our own transaction and take care of that here.
+	    FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+	    Fragment prev = getSupportFragmentManager().findFragmentByTag("dialog");
+	    if (prev != null) {
+	        ft.remove(prev);
+	    }
+	    ft.addToBackStack(null);
+
+	    // Create and show the dialog.
+	    DialogFragment newFragment = MyDialogFragment.newInstance(mStackLevel);
+	    newFragment.show(ft, "dialog");
+	}
+	
+	public void callback() {
+		Toast.makeText(this, "GOT CALLBACK", Toast.LENGTH_SHORT).show();
+	}
+}
---
+++ src/com/subha/modals/MyDialogFragment.java
@@ -0,0 +1,55 @@
+package com.subha.modals;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+public class MyDialogFragment extends DialogFragment {
+	int mNum;
+
+    /**
+     * Create a new instance of MyDialogFragment, providing "num"
+     * as an argument.
+     */
+    static MyDialogFragment newInstance(int num) {
+        MyDialogFragment f = new MyDialogFragment();
+
+        // Supply num input as an argument.
+        Bundle args = new Bundle();
+        args.putInt("num", num);
+        f.setArguments(args);
+
+        return f;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mNum = getArguments().getInt("num");
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_dialog, container, false);
+        View tv = v.findViewById(R.id.tvText);
+        ((TextView)tv).setText("Dialog #" + mNum);
+
+        // Watch for button clicks.
+        Button button = (Button)v.findViewById(R.id.btnSave);
+        button.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                // When button is clicked, call up to owning activity.
+                ((MainActivity)getActivity()).callback();
+                dismiss();
+            }
+        });
+
+        return v;
+    }
+}
